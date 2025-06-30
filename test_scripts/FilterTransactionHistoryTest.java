09a241e877f1aa651f3fc85e86ce981c71e18f74
AnubrotoGhose <anubroto.g@prodapt.com>
2025-06-30 15:08:15 +0530

    Generated complete test implementation for BANK-2637 - 20250630_150812

--- test_scripts/FilterTransactionHistoryTest.java
+++ test_scripts/FilterTransactionHistoryTest.java
@@ -1,26 +1,30 @@
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 @SpringBootTest
-@AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
 public class FilterTransactionHistoryTest {
-    @Autowired
-    private WebDriver driver;
 
-    @Test
-    public void testFilterTransactionHistoryByDateRange() {
-        driver.get("http://banking-portal.com/transaction-history");
-        driver.findElement(By.id("date-range-start")).sendKeys("2022-01-01");
-        driver.findElement(By.id("date-range-end")).sendKeys("2022-12-31");
-        driver.findElement(By.id("apply-filter-btn")).click();
+	@Autowired
+	private WebDriver driver;
 
-        // Add assertions to validate filtered results
-    }
+	@MockBean
+	private UserService userService;
+
+	@Test
+	public void testFilterTransactionHistoryByDateRange() {
+		driver.get("https://bankingportal.com");
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login("username", "password");
+
+		TransactionHistoryPage transactionHistoryPage = new TransactionHistoryPage(driver);
+		transactionHistoryPage.navigateTo();
+
+		DateRangeFilter dateRangeFilter = new DateRangeFilter("01/01/2021", "01/31/2021");
+		transactionHistoryPage.applyDateRangeFilter(dateRangeFilter);
+
+		List<Transaction> filteredTransactions = transactionHistoryPage.getFilteredTransactions();
+
+		for (Transaction transaction : filteredTransactions) {
+			assertTrue(transaction.getDate().after(dateRangeFilter.getStartDate()), "Transaction date should be after start date");
+			assertTrue(transaction.getDate().before(dateRangeFilter.getEndDate()), "Transaction date should be before end date");
+		}
+	}
 }
