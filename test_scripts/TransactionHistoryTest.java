34bfbd7b4bf442d824b76c1ed0136616b745c73d
AnubrotoGhose <anubroto.g@prodapt.com>
2025-06-30 11:13:10 +0530

    Generated complete test implementation for BANK-2640 - 20250630_111308

---
+++ test_scripts/TransactionHistoryTest.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class TransactionHistoryTest {
+    @Test
+    public void testRetrieveTransactionHistory() {
+        // Implement test logic here
+    }
+}
