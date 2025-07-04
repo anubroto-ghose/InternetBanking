23f0f415ea0e3819f8abad469149ddead877f47c
AnubrotoGhose <anubroto.g@prodapt.com>
2025-07-04 12:50:36 +0530

    Generated Selenium test for BANK-2298 - 20250704_125136 (attempt 1)

--- test_scripts/CurrencyConversionTest.java
+++ test_scripts/CurrencyConversionTest.java
@@ -1,6 +1,8 @@
 package com.webapp.bankingportal;
 
-import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,63 +11,59 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.bind.annotation.RequestBody;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
+@ExtendWith({SpringExtension.class, MockitoExtension.class})
 @SpringBootTest
-@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc
 public class CurrencyConversionTest {
 
+    @Mock
+    private AccountService accountService;
+    
+    @InjectMocks
+    private UserRepository userRepository;
+    
     private WebDriver driver;
 
-    @MockBean
-    private AccountService accountService;
-
-    @MockBean
-    private TokenService tokenService;
-
-    @MockBean
-    private UserRepository userRepository;
-
     @BeforeEach
-    public void setUp() {
+    public void setup() {
         System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
         driver = new ChromeDriver();
         driver.get("http://localhost:8080/bankingportal");
     }
 
+    @Test
+    public void testCurrencyConversionDecimalPrecision() {
+        // Mocking the service response
+        double inputAmount = 1234.56;
+        double expectedOutput = 15.67; // Assume this is the expected conversion result
+        when(accountService.convertCurrency(inputAmount, "INR", "USD")).thenReturn(expectedOutput);
+
+        // Input amount in INR
+        WebElement inputField = driver.findElement(By.id("amountInINR"));
+        inputField.sendKeys(String.valueOf(inputAmount));
+
+        // Trigger currency conversion
+        WebElement convertButton = driver.findElement(By.id("convertButton"));
+        convertButton.click();
+
+        // Verify the converted amount in USD
+        WebElement resultField = driver.findElement(By.id("resultInUSD"));
+        double actualOutput = Double.parseDouble(resultField.getText());
+
+        // Assertion
+        assertEquals(expectedOutput, actualOutput, 0.01);
+    }
+
     @AfterEach
     public void tearDown() {
         driver.quit();
-    }
-
-    @Test
-    public void testCurrencyConversionPrecision() {
-        // Mocking the currency conversion response
-        double inputAmount = 1234.56;
-        double expectedConvertedAmount = 16.67; // Example expected value
-        when(accountService.convertCurrency(inputAmount, "INR", "USD"))
-            .thenReturn(new ResponseEntity<>(expectedConvertedAmount, HttpStatus.OK));
-
-        // Inputting amount in INR
-        driver.findElement(By.id("amountInput"))
-              .sendKeys(String.valueOf(inputAmount));
-
-        // Triggering conversion
-        driver.findElement(By.id("convertButton")).click();
-
-        // Verifying the converted amount
-        String convertedAmountText = driver.findElement(By.id("convertedAmount"))
-                                         .getText();
-        double convertedAmount = Double.parseDouble(convertedAmountText);
-
-        assertEquals(expectedConvertedAmount, convertedAmount, 0.01, "The converted amount does not match the expected precision.");
     }
 }
