7edd2195b233c7680236ef22c4fbb62283b2b119
AnubrotoGhose <anubroto.g@prodapt.com>
2025-07-09 15:11:49 +0530

    Generated Selenium test for BANK-3316 in Epic BANK-3298 - 20250709_094129

---
+++ test_scripts/BANK-3298/BANK-3316/ForexTransactionTest.java
@@ -0,0 +1,27 @@
+/**
+ * Test Case ID: TEST_CASE
+ * Generated from Jira Ticket: BANK-3316
+ * Epic: BANK-3298
+ * Generated on: 2025-07-09 09:41:29
+ * 
+ * This is an auto-generated Selenium test script.
+ * Modify with caution as changes may be overwritten.
+ */
+
+package com.webapp.bankingportal;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ForexTransactionTest {
+
+    private WebDriver driver;
+
+    @Test
+    public void testFailedForexTransactionDueToInsufficientBalance() {
+        // Test Steps: Add Selenium WebDriver code here
+
+        // Expected Results: Add assertions here
+    }
+}
