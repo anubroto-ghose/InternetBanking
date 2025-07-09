d1f0ef6c86ad65cdb0c469b16c2931ee5a395db4
AnubrotoGhose <anubroto.g@prodapt.com>
2025-07-09 15:04:57 +0530

    Generated Selenium test for BANK-3334 in Epic BANK-3299 - 20250709_093456

---
+++ test_scripts/BANK-3299/BANK-3334/LoginIntegrationTest.java
@@ -0,0 +1,28 @@
+/**
+ * Test Case ID: TEST_CASE
+ * Generated from Jira Ticket: BANK-3334
+ * Epic: BANK-3299
+ * Generated on: 2025-07-09 09:34:56
+ * 
+ * This is an auto-generated Selenium test script.
+ * Modify with caution as changes may be overwritten.
+ */
+
+package com.webapp.bankingportal;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LoginIntegrationTest {
+
+    private WebDriver driver;
+
+    @Test
+    public void testLoginSuccess() {
+        // Start browser, navigate to login page, enter valid credentials, click login button
+        // Assert user is logged in, redirected to dashboard, and no errors displayed
+    }
+}
