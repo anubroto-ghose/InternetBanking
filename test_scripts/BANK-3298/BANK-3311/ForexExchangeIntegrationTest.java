4f987571f6ea75851e79d4e43ed2ccc6e0f7ef7f
AnubrotoGhose <anubroto.g@prodapt.com>
2025-07-09 15:16:04 +0530

    Generated Selenium test for BANK-3311 in Epic BANK-3298 - 20250709_094550

---
+++ test_scripts/BANK-3298/BANK-3311/ForexExchangeIntegrationTest.java
@@ -0,0 +1,35 @@
+/**
+ * Test Case ID: TEST_CASE
+ * Generated from Jira Ticket: BANK-3311
+ * Epic: BANK-3298
+ * Generated on: 2025-07-09 09:45:50
+ * 
+ * This is an auto-generated Selenium test script.
+ * Modify with caution as changes may be overwritten.
+ */
+
+package com.webapp.bankingportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@SpringBootTest
+public class ForexExchangeIntegrationTest {
+    private WebDriver driver;
+
+    @Test
+    public void testForexExchangeWithMaximumAmount() {
+        // Initialize WebDriver
+        driver = new ChromeDriver();
+
+        // Write test steps using Selenium
+        // This test will interact with the UI to perform forex exchange
+
+        // Add assertions for expected results
+
+        // Close the WebDriver
+        driver.quit();
+    }
+}
