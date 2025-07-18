f14545de21eaf1617ad9ea95c19cd27882935fe9
AnubrotoGhose <anubroto.g@prodapt.com>
2025-07-18 16:46:55 +0530

    Generated Selenium test for BANK-3966 in Epic BANK-3931 - 20250718_164652

---
+++ test_scripts/BANK-3931/BANK-3966/SpringBootIntegrationTest.java
@@ -0,0 +1,23 @@
+/**
+ * Test Case ID: TEST_CASE
+ * Generated from Jira Ticket: BANK-3966
+ * Epic: BANK-3931
+ * Generated on: 2025-07-18 16:46:52
+ * 
+ * This is an auto-generated Selenium test script.
+ * Modify with caution as changes may be overwritten.
+ */
+
+package com.webapp.fpmbackend.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntegrationTest {
+
+    @Test
+    void testIntegrationWithWebDriver() {
+        // Add test logic here
+    }
+}
