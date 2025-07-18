9e47de8a8a93e10eac8d30bf562d3b4026c7eb6a
AnubrotoGhose <anubroto.g@prodapt.com>
2025-07-18 16:27:08 +0530

    Generated Selenium test for BANK-3944 in Epic BANK-3930 - 20250718_105659

---
+++ test_scripts/BANK-3930/BANK-3944/FPMIntegrationTest.java
@@ -0,0 +1,23 @@
+/**
+ * Test Case ID: TEST_CASE
+ * Generated from Jira Ticket: BANK-3944
+ * Epic: BANK-3930
+ * Generated on: 2025-07-18 10:56:59
+ * 
+ * This is an auto-generated Selenium test script.
+ * Modify with caution as changes may be overwritten.
+ */
+
+package com.webapp.fpmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FPMIntegrationTest {
+
+    @Test
+    void testFpmIntegration() {
+        // Add test implementation here
+    }
+}
