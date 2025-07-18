41b9723e7b5a049610e674610f1bbb37123512d8
AnubrotoGhose <anubroto.g@prodapt.com>
2025-07-18 16:24:52 +0530

    Generated Selenium test for BANK-3949 in Epic BANK-3930 - 20250718_105445

---
+++ test_scripts/BANK-3930/BANK-3949/FpmToolsIntegrationTest.java
@@ -0,0 +1,38 @@
+/**
+ * Test Case ID: TEST_CASE
+ * Generated from Jira Ticket: BANK-3949
+ * Epic: BANK-3930
+ * Generated on: 2025-07-18 10:54:45
+ * 
+ * This is an auto-generated Selenium test script.
+ * Modify with caution as changes may be overwritten.
+ */
+
+package com.webapp.fpmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FpmToolsIntegrationTest {
+
+    @Test
+    void testLoginAsApproverAndApproveFinancialEntry() {
+        // Test implementation here
+    }
+
+    @Test
+    void testNavigateToPendingApprovalsSection() {
+        // Test implementation here
+    }
+
+    @Test
+    void testSelectFinancialEntryToApprove() {
+        // Test implementation here
+    }
+
+    @Test
+    void testDocumentReasonsForApproval() {
+        // Test implementation here
+    }
+}
