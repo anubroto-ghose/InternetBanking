44dea9b057a56b1554f5a063b725293645317361
AnubrotoGhose <anubroto.g@prodapt.com>
2025-07-18 16:26:16 +0530

    Generated Selenium test for BANK-3946 in Epic BANK-3930 - 20250718_105608

---
+++ test_scripts/BANK-3930/BANK-3946/FpmCurrencyRateIntegrationTest.java
@@ -0,0 +1,23 @@
+/**
+ * Test Case ID: TEST_CASE
+ * Generated from Jira Ticket: BANK-3946
+ * Epic: BANK-3930
+ * Generated on: 2025-07-18 10:56:08
+ * 
+ * This is an auto-generated Selenium test script.
+ * Modify with caution as changes may be overwritten.
+ */
+
+package com.webapp.fpmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FpmCurrencyRateIntegrationTest {
+
+    @Test
+    public void testRetrieveHistoricalCurrencyRates() {
+        // Test steps here
+    }
+}
