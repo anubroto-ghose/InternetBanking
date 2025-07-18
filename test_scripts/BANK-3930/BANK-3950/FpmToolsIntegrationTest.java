b7ec746561599105df33c2973376a7a59484ec62
AnubrotoGhose <anubroto.g@prodapt.com>
2025-07-18 16:24:25 +0530

    Generated Selenium test for BANK-3950 in Epic BANK-3930 - 20250718_105417

---
+++ test_scripts/BANK-3930/BANK-3950/FpmToolsIntegrationTest.java
@@ -0,0 +1,28 @@
+/**
+ * Test Case ID: TEST_CASE
+ * Generated from Jira Ticket: BANK-3950
+ * Epic: BANK-3930
+ * Generated on: 2025-07-18 10:54:17
+ * 
+ * This is an auto-generated Selenium test script.
+ * Modify with caution as changes may be overwritten.
+ */
+
+package com.webapp.fpmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class FpmToolsIntegrationTest {
+
+    @Test
+    void testFinancialApprovalWorkflow() {
+        // Write your Selenium integration test code here
+    }
+
+}
